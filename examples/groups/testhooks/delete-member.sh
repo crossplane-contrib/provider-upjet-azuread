da81dc3963b760d700265c37c4fecf12eb6d50fe
Fatih Türken <turkenf@gmail.com>
2022-12-09 18:19:29 +0300

    Add testhooks for azuread_group_member

---
+++ examples/groups/testhooks/delete-member.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -aeuo pipefail
+
+# Note(turkenf): We are getting "Resource '5f5fdd0e-e056-4c4d-a407-43d80e5c11b5' does not exist or 
+# one of its queried reference-property objects are not present." exception if member for
+# the group got deleted before the member resource. This is a workaround for this
+# problem to make automated tests to work.
+${KUBECTL} delete member.groups.azuread.upbound.io -l testing.upbound.io/example-name=example
