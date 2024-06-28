a90ad2e782d4f4838fc67c86695405100bc6fbea
Christian Artin <gravufo@gmail.com>
2024-06-28 14:54:10 -0400

    Add support for ManagedIdentity and OIDC authentication methods
    Signed-off-by: Christian Artin <gravufo@gmail.com>

--- apis/v1beta1/types.go
+++ apis/v1beta1/types.go
@@ -14,12 +14,46 @@ import (
 type ProviderConfigSpec struct {
 	// Credentials required to authenticate to this provider.
 	Credentials ProviderCredentials `json:"credentials"`
+
+	// ClientID is the user-assigned managed identity's ID
+	// when Credentials.Source is `InjectedIdentity`. If unset and
+	// Credentials.Source is `InjectedIdentity`, then a system-assigned
+	// managed identity is used.
+	// +optional
+	ClientID *string `json:"clientID,omitempty"`
+
+	// SubscriptionID is the Azure subscription ID to be used.
+	// If unset, subscription ID from Credentials will be used.
+	// Required if Credentials.Source is InjectedIdentity.
+	// +kubebuilder:validation:Optional
+	SubscriptionID *string `json:"subscriptionID,omitempty"`
+
+	// TenantID is the Azure AD tenant ID to be used.
+	// If unset, tenant ID from Credentials will be used.
+	// Required if Credentials.Source is InjectedIdentity.
+	// +kubebuilder:validation:Optional
+	TenantID *string `json:"tenantID,omitempty"`
+
+	// MSIEndpoint is the optional path to a custom endpoint for
+	// Managed Service Identity.
+	// +kubebuilder:validation:Optional
+	MSIEndpoint *string `json:"msiEndpoint,omitempty"`
+
+	// The Cloud Environment which should be used. Possible values are "public",
+	// "usgovernment", "german", and "china". Defaults to "public".
+	// +kubebuilder:validation:Optional
+	Environment *string `json:"environment,omitempty"`
+
+	// OIDCTokenFilePath is the optional path to a token file
+	// that allows to access a managed identity.
+	// +kubebuilder:validation:Optional
+	OidcTokenFilePath *string `json:"oidcTokenFilePath,omitempty"`
 }
 
 // ProviderCredentials required to authenticate.
 type ProviderCredentials struct {
 	// Source of the provider credentials.
-	// +kubebuilder:validation:Enum=None;Secret;InjectedIdentity;Environment;Filesystem
+	// +kubebuilder:validation:Enum=None;Secret;UserAssignedManagedIdentity;SystemAssignedManagedIdentity;OIDCTokenFile
 	Source xpv1.CredentialsSource `json:"source"`
 
 	xpv1.CommonCredentialSelectors `json:",inline"`
@@ -32,12 +66,13 @@ type ProviderConfigStatus struct {
 
 // +kubebuilder:object:root=true
 
-// A ProviderConfig configures a Azuread provider.
+// A ProviderConfig configures the AzureAD provider.
 // +kubebuilder:subresource:status
 // +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
 // +kubebuilder:printcolumn:name="SECRET-NAME",type="string",JSONPath=".spec.credentials.secretRef.name",priority=1
 // +kubebuilder:resource:scope=Cluster
 // +kubebuilder:resource:scope=Cluster,categories={crossplane,provider,azuread}
+// +kubebuilder:storageversion
 type ProviderConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -63,6 +98,7 @@ type ProviderConfigList struct {
 // +kubebuilder:printcolumn:name="RESOURCE-KIND",type="string",JSONPath=".resourceRef.kind"
 // +kubebuilder:printcolumn:name="RESOURCE-NAME",type="string",JSONPath=".resourceRef.name"
 // +kubebuilder:resource:scope=Cluster,categories={crossplane,provider,azuread}
+// +kubebuilder:storageversion
 type ProviderConfigUsage struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
